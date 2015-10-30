2f12b7e4b1925db28d95bfcf74e5bc933db0d70d
Lianfen Shi <lianfen.shi@f1006-13-34173.bmcc.cuny.edu>
2015-10-29 21:18:31 -0400

    add plugin post

--- index.php
+++ index.php
@@ -48,6 +48,35 @@ get_header(); ?>
 
 			<?php endwhile; ?>
 
+
+
+
+
+			<?php rewind_posts(); ?>
+
+<?php
+
+//$args = array( ‘post_type’ => ‘photo_item’, ‘posts_per_page’ => 10 );
+$args = array('post_type' => 'photo_item', 'posts_per_page' => 10);
+$loop = new WP_Query( $args );
+while ( $loop->have_posts() ) : $loop->the_post();
+
+
+the_title();
+echo '<div class=”entry-content”>';
+the_content();
+echo '</div>';
+echo '<div class="plug-content">';
+the_content();
+echo '</div>';
+
+endwhile;
+
+?>
+
+
+
+
 			<?php the_posts_navigation(); ?>
 
 		<?php else : ?>
